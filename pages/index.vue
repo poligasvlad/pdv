9596cc6dc5ab545b5831de2b45e24294c5296f70
poligasvlad <poligasvlad@gmail.com>
2022-05-10 15:50:41 +0300

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,26 @@
     <Header></Header>
     <main class="main">
       <div class="container">
-
+        <div class="wrapper">
+          <h1>
+            Оцінка A? - Тоді Вам на As’ hub.<br>
+            Попіклуємось про ваші дедлайни самі!
+          </h1>
+          <p>
+            Вітаємо на As’ hub!<br>
+            Ми ті, хто допоможе Вам покращити успішність в школі, університеті та інших навчальних закладах.<br>
+            Реферати, статті, курсові роботи, презентації та інші роботи, які виконають за тебе.<br>
+            Оформлюйте своє замовлення прямо зараз та отримайте якісну роботу, відповідно до встановлених Вами дедлайнами.<br>
+          </p>
+          <div class="btn-wrapper">
+            <button class="btn">Замовити роботу</button>
+            <button class="btn">Стати автором</button>
+          </div>
+        </div>
       </div>
     </main>
   </div>
 </template>
-
 <script>
 export default {
   layout: 'default',
@@ -20,9 +34,6 @@ export default {
 </script>
 
 <style>
-.container{
-  width: 95%;
-  margin: auto;
-}
+@import '~/assets/styles/pages/index.scss';
 </style>
 
